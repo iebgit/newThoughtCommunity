b36646803b5846bea7f8665bdca2fc557844c452
iebgit <iebdev84@gmail.com>
2022-04-13 15:56:19 -0400

    removed x-padding for better responsiveness on phones

--- new-thought/pages/index.tsx
+++ new-thought/pages/index.tsx
@@ -22,7 +22,7 @@ export default function Home({posts}: Props) {
       </Head>
 
       <Header />
-      <div className='flex justify-between items-center bg-yellow-400 border-y border-black py-10 px-20'>
+      <div className='flex justify-between items-center bg-yellow-400 border-y border-black py-10'>
         <div className='px-10 space-y-5'>
           <h1 className='text-6xl  max-w-xl font-serif'>
             <span className='underline decoration-black decoration-4'>
